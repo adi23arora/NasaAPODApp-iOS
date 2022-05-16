bc7db44bdf9ad0a6b4c64ce785e05a6880520cf7
Aditya Arora <aadityaaaaroraa@gmail.com>
2022-05-16 18:24:57 +0530

    Add NasaPODService to fetch data from Nasa APOD api

---
+++ NasaPODApp/Service/NasaPODService.swift
@@ -0,0 +1,42 @@
+//
+//  NasaPODService.swift
+//  NasaPODApp
+//
+//  Created by Aditya Arora on 16/05/22.
+//
+
+import UIKit
+
+class NasaPODService {
+    struct APIConstants {
+        static let podUrl = "https://api.nasa.gov/planetary/apod?api_key=PSGjqKkc2MtgH7pskf4FHfgS5JSxsyFGnnPYhmTZ"
+    }
+
+    func fetchPODDetails(completion: @escaping (NasaPODResponse?) -> Void) {
+        HTTPClient.shared.getRequest(url: APIConstants.podUrl) { result in
+            if case .success(let data) = result {
+                do {
+                    let podResponse = try JSONDecoder().decode(NasaPODResponse.self,
+                                                               from: data)
+                    completion(podResponse)
+                } catch {
+                    completion(nil)
+                }
+            } else {
+                completion(nil)
+            }
+        }
+    }
+
+    func fetchPODImage(imageUrl: String, completion: @escaping (UIImage?) -> Void) {
+        HTTPClient.shared.getRequest(url: imageUrl) { result in
+            if case .success(let data) = result {
+                let image = UIImage(data: data)
+                completion(image)
+            } else {
+                completion(nil)
+            }
+        }
+    }
+
+}
